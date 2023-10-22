3f71ac4634f16f3cb029a49627b2f21de857a887
ibrahimaq <ibrahim_aq@hotmail.co.uk>
2023-10-22 15:04:31 +0100

    api docker-compose set up

---
+++ api/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (req,res) => {
+  res.json([
+    {
+      "id":"1",
+      "title":"Book Review: The Bear & The Nightingale"
+    },
+    {
+      "id":"2",
+      "title":"Game Review: Pokemon Brillian Diamond"
+    },
+    {
+      "id":"3",
+      "title":"Show Review: Alice in Borderland"
+    }
+  ])
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
